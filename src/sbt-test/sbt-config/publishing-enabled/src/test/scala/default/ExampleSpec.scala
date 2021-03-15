c69c44d763671935cb22c8f402e3f63f3107e361
Eric Peters <eric.peters@dotdata.com>
2021-03-15 10:10:44 -0700

    Rename SbtConfig to SbtConfigPlugin, change some in to /, and change the format conf files to be managed by resourceGenerators

--- src/sbt-test/sbt-config/publishing-enabled/src/test/scala/default/ExampleSpec.scala
+++ src/sbt-test/sbt-config/publishing-enabled/src/test/scala/default/ExampleSpec.scala
@@ -1,6 +1,6 @@
 package default
 
-import collection.mutable.Stack
+import collection.mutable.ArrayStack
 import org.scalatest._
 import flatspec._
 import matchers._
@@ -8,17 +8,17 @@ import matchers._
 class ExampleSpec extends AnyFlatSpec with should.Matchers {
 
   "A Stack" should "pop values in last-in-first-out order" in {
-    val stack = new Stack[Int]
+    val stack = new ArrayStack[Int]
     stack.push(1)
     stack.push(2)
-    stack.pop() should be (2)
-    stack.pop() should be (1)
+    stack.pop() should be(2)
+    stack.pop() should be(1)
   }
 
   it should "throw NoSuchElementException if an empty stack is popped" in {
-    val emptyStack = new Stack[Int]
-    a [NoSuchElementException] should be thrownBy {
+    val emptyStack = new ArrayStack[Int]
+    a[RuntimeException] should be thrownBy {
       emptyStack.pop()
-    } 
+    }
   }
 }
--- src/sbt-test/sbt-config/scalafmt/src/test/scala/default/ExampleSpec.scala
+++ src/sbt-test/sbt-config/scalafmt/src/test/scala/default/ExampleSpec.scala
@@ -1,6 +1,6 @@
 package default
 
-import collection.mutable.Stack
+import collection.mutable.ArrayStack
 import org.scalatest._
 import flatspec._
 import matchers._
@@ -8,7 +8,7 @@ import matchers._
 class ExampleSpec extends AnyFlatSpec with should.Matchers {
 
   "A Stack" should "pop values in last-in-first-out order" in {
-    val stack = new Stack[Int]
+    val stack = new ArrayStack[Int]
     stack.push(1)
     stack.push(2)
     stack.pop() should be(2)
@@ -16,8 +16,8 @@ class ExampleSpec extends AnyFlatSpec with should.Matchers {
   }
 
   it should "throw NoSuchElementException if an empty stack is popped" in {
-    val emptyStack = new Stack[Int]
-    a[NoSuchElementException] should be thrownBy {
+    val emptyStack = new ArrayStack[Int]
+    a[RuntimeException] should be thrownBy {
       emptyStack.pop()
     }
   }
